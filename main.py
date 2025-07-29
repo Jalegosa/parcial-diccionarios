63333a58655e502f46ece30f7c74cb5b232679d9
Jalegosa <jalegosa01@gmail.com>
2025-07-28 18:50:50 -0600

    prueba1

--- main.py
+++ main.py
@@ -17,11 +17,18 @@ empleados = {
     }
 
     }
-input("\n CONTROL DE EMPLEADOS:")
+print(" CONTROL DE EMPLEADOS:")
 codigo = input("Ingrese el código del Empleado: ")
 nombre = input("Ingrese el nombre completo: ")
 departamento = input("Ingrese el departamento en el que trabaja: ")
+#print(f"El empleado {nombre} con codigo {codigo} trabaja en {departamento}")
+
+print(f"\n Para conocer el desempeño del empleado {nombre} por favor, responda:")
+puntualidad = input("\nPuntualidad (0-10):  ")
+equipo = input("\nTrabajo en Equio (0-10): ")
+productividad = input("\nNivel de Productividad (0-10): ")
 
 
 
-print(f"El empleado {nombre} con codigo {codigo} trabaja en {departamento}")
+
+
