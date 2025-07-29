2d631108f3f818263481363c769df2b5b13b3820
Jalegosa <jalegosa01@gmail.com>
2025-07-28 19:15:37 -0600

    prueba1

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ empleados = {
           "equipo": "equipo",
           "productividad": "productividad",
           "observaciones": "observaciones",
-          "promedio" : "promedio"
+          "promedio" : "promedio",
+          "estado": "estado",
       },
         "contacto":{
             "telefono" : "telefono",
@@ -18,9 +19,9 @@ empleados = {
 
     }
 
-def resultado_promedio(calificacion)
-
-    return 0
+#def resultado_promedio(calificacion):
+#    calificacion = puntualidad + equipo + productividad
+# return 0
 
 
 
@@ -28,14 +29,41 @@ print(" CONTROL DE EMPLEADOS:")
 codigo = input("Ingrese el código del Empleado: ")
 nombre = input("Ingrese el nombre completo: ")
 departamento = input("Ingrese el departamento en el que trabaja: ")
+antiguedad = input("Ingrese la antiguedad del empleado: ")
+
 #print(f"El empleado {nombre} con codigo {codigo} trabaja en {departamento}")
 
 print(f"\n Para conocer el desempeño del empleado {nombre} por favor, responda:")
-puntualidad = input("\nPuntualidad (0-10):  ")
-equipo = input("\nTrabajo en Equio (0-10): ")
+puntualidad = int (input("\nPuntualidad (0-10):  "))
+equipo = input("\nTrabajo en Equipo (0-10): ")
 productividad = input("\nNivel de Productividad (0-10): ")
 observaciones = input("Agregue sus observaciones :")
 
+print(f"\n Por favor ingrese la información de contacto de {nombre}:")
+telefono= input("\nNúmero telefónico: ")
+correo = input("Correo electrónico :")
+
+#op = input("¿Desea ver la información completa del empleado? (si/no) ").upper()
+#if op == "si":
+#     mostrar.empleado()
+#else:
+#    exit()
+
+
+print("\nBúsqueda de empleados")
+empleado_buscado = input("Ingrese el codigo de empleado que desea buscar: ")
+
+if empleado_buscado in empleados:
+    empleado = empleados[empleado_buscado]
+    print("\nEmpleado encontrado:")
+    print(f"Nombre: {empleado['nombre']}")
+    print(f"Departamento: {empleado['departamento']}")
+    print(f"Antiguedad: {empleado['antiguedad']}")
+    print(f"Evaluacion: {empleado['evaluacion']['estado']}")
+    print(f"Correo: {empleado['contacto']['correo']}")
+    print(f"Teléfono: {empleado['contacto']['telefono']}")
+else:
+    print("Empleado no encontrado.")
 
 
 
