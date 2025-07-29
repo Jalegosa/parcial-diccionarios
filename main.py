06eb42998ad49a97cb48eb1ad0fd0ed45dc6daab
Jalegosa <jalegosa01@gmail.com>
2025-07-28 18:41:53 -0600

    prueba1

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-empleados ={
-    "codigo":{
+empleados = {
+    "codigo": {
+    "nombre": "nombre",
+     "departamento" : "departamento",
+     "antiguedad": "antiguedad",
+      "evaluacion": {
+          "puntualidad": "puntualidad",
+          "equipo": "equipo",
+          "productividad": "productividad",
+          "observaciones": "observaciones",
+          "promedio" : "promedio"
+      },
+        "contacto":{
+            "telefono" : "telefono",
+            "correo" : "correo"
+        }
+    }
 
     }
-}
+input("\n CONTROL DE EMPLEADOS:")
+codigo = input("Ingrese el código del Empleado: ")
+nombre = input("Ingrese el nombre completo: ")
+departamento = input("Ingrese el departamento en el que trabaja: ")
+
+
+
+print(f"El empleado {nombre} con codigo {codigo} trabaja en {departamento}")
