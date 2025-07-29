f9cddadf85bba6021aea615b563b57669066c45d
Jalegosa <jalegosa01@gmail.com>
2025-07-28 18:53:50 -0600

    prueba1

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ empleados = {
     }
 
     }
+
+def resultado_promedio(calificacion)
+
+
 print(" CONTROL DE EMPLEADOS:")
 codigo = input("Ingrese el código del Empleado: ")
 nombre = input("Ingrese el nombre completo: ")
@@ -27,6 +31,8 @@ print(f"\n Para conocer el desempeño del empleado {nombre} por favor, responda:
 puntualidad = input("\nPuntualidad (0-10):  ")
 equipo = input("\nTrabajo en Equio (0-10): ")
 productividad = input("\nNivel de Productividad (0-10): ")
+observaciones = input("Agregue sus observaciones :")
+
 
 
 
